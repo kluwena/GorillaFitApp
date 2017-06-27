b35d337e750b1b6c59cee8ba96254345b7f942fb
kluwena <karelluwena@gmail.com>
2017-06-26 22:04:08 -0700

    added another exerciseentry for dummy data

--- client/src/components/Exercises.jsx
+++ client/src/components/Exercises.jsx
@@ -4,7 +4,8 @@ import ExerciseEntry from './ExerciseEntry.jsx';
 const Exercises = (props) => (
   <div>
     <h4>Exercises You've Done Today: </h4>
-    <ExerciseEntry exercise={'push ups'}/>
+    <ExerciseEntry exercise={'push ups'} />
+    <ExerciseEntry exercise={'yoga'} />
   </div>
 );
 
