ca642f78ba8f2e6cdced76bd50ecc6af77df4d8e
kluwena <karelluwena@gmail.com>
2017-06-26 21:50:21 -0700

    made baseline skeleton for exercise and exerciseentry

---
+++ client/src/components/Exercises.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ExerciseEntry from './ExerciseEntry.jsx';
+
+const Exercises = (props) => {
+  <div>
+    <ExerciseEntry />
+  </div>;
+};
+
+export default Exercises;
